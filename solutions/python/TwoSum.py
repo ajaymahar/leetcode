7c3e37d9103a16d90ec60d611d76cfb74cdba2aa
ajaymahar <ajay.mhar@gmail.com>
2020-11-27 02:26:14 +0530

    TwoSum problem python solution.

---
+++ solutions/python/TwoSum.py
@@ -0,0 +1,49 @@
+class Solution:
+    def twoSum(self, nums, target):
+        '''
+        Method-1: Bruteforce
+
+        time compx: O(n^2)
+        space compx: O(1)
+
+        def twoSum(self, nums, targer):
+            """TODO: Docstring for bruteForceTwoSum.
+
+            :function: twoSum()
+            :returns: List of index
+
+            """
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums), 1):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+        Method-2:
+        time conplex: O(n) -> in worst case we have to visit all the element
+        from the list.
+        space compx: O(n)
+
+        approach would be
+        1. create empty dict to keep track of visited numbers.
+        2. loop through all the elements from the list and get
+            the index and item
+        3. check if target - curent-numner present in dict
+        4. if yes then simply return the stored index and current index.
+        5. if not then store the current value and index to the dict.
+
+        '''
+
+        # enumerate will return the index and the value
+        visited = {}  # keep track of all visited numbers
+        for idx, num in enumerate(nums):
+            # checking if we have values in visited dict.
+            if target - num in visited:
+                return [visited.get(target - num), idx]
+            visited[num] = idx  # if not add it to dict
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 15], 9))
